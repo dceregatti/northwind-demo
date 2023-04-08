be4178b18ac9892beec526ca5502739845157b25
dceregatti@gmail.com <dceregatti@gmail.com>
2023-04-08 15:42:34 +0000

    Add sales fact table, docs and test file

--- models/marts/dim_produtos.sql
+++ models/marts/dim_produtos.sql
@@ -22,23 +22,24 @@ with produtos as (
 ,join_tabelas as (
 
     select  produtos.id_produto
-            ,produtos.id_fornecedor
-            ,produtos.id_categoria
-            ,produtos.nome_produto
-            ,produtos.quantidade_por_unidade
-            ,produtos.preco_por_unidade
-            ,produtos.unidades_em_estoque
-            ,produtos.unidades_por_ordem
-            ,produtos.nivel_reabastecimento
-            ,categorias.nome_categoria
-            ,categorias.descricao_categoria
-            ,fornecedores.nome_fornecedor
-            ,fornecedores.contato_fornecedor
-            ,fornecedores.endereco_fornecedor
-            ,fornecedores.cep_fornecedor
-            ,fornecedores.cidade_fornecedor
-            ,fornecedores.regiao_fornecedor
-            ,fornecedores.pais_fornecedor
+   , produtos.id_fornecedor
+            , produtos.id_categoria
+            , produtos.nome_produto
+            , produtos.quantidade_por_unidade
+            , produtos.preco_por_unidade
+            , produtos.unidades_em_estoque
+            , produtos.unidades_por_ordem
+            , produtos.nivel_reabastecimento
+            , produtos.is_discontinuado
+            , categorias.nome_categoria
+            , categorias.descricao_categoria
+            , fornecedores.nome_fornecedor
+            , fornecedores.contato_fornecedor
+            , fornecedores.endereco_fornecedor
+            , fornecedores.cep_fornecedor
+            , fornecedores.cidade_fornecedor
+            , fornecedores.regiao_fornecedor
+            , fornecedores.pais_fornecedor
     from produtos
     left join categorias
     on produtos.id_categoria = categorias.id_categoria
